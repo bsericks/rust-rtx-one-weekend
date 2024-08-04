c89cc53d710b4e88177b45bb9c443acbb9d7bfa3
Brian Erickson <berickson@nvidia.com>
2024-08-04 13:16:53 -0500

    finished 11.3

--- src/material.rs
+++ src/material.rs
@@ -2,7 +2,7 @@ use crate::color::color::Color;
 use crate::hitable::HitRecord;
 use crate::ray::ray::Ray;
 use crate::rtweekend;
-use crate::vec3::{random_unit_vector, reflect};
+use crate::vec3::{random_unit_vector, reflect, refract, unit_vector, Vec3};
 
 pub trait Material: Sync + Send {
     fn scatter(
@@ -66,6 +66,54 @@ impl Material for Metal {
 
         *scattered = Ray::new(rec.p, reflected);
         *attenuation = self.albedo;
+        true
+    }
+}
+
+pub struct Dielectric
+{
+    refraction_index: f32,
+}
+
+impl Dielectric {
+    pub fn new(refraction_index: f32) -> Dielectric {
+        Dielectric { refraction_index }
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(
+        &self,
+        r_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Color,
+        scattered: &mut Ray,
+    ) -> bool {
+        *attenuation = Color::new(1.0, 1.0, 1.0);
+        let mut ri = self.refraction_index;
+        if rec.front_face {
+            ri = 1.0/self.refraction_index;
+        }
+
+        let unit_direction = unit_vector(*r_in.direction());
+        let cos_theta = (-unit_direction.dot(rec.normal).min(1.0));
+        let sin_theta = (1.0 - cos_theta*cos_theta).sqrt();
+
+        let cannot_refract = ri*sin_theta > 1.0;
+        
+        //let refracted = refract(&unit_direction, &rec.normal, ri);
+
+        let mut _direction = Vec3::new(1.0, 1.0, 1.0);
+
+        if cannot_refract {
+            _direction = reflect(&unit_direction, &rec.normal);
+        }
+        else {
+            _direction = refract(&unit_direction, &rec.normal, ri);
+        }
+
+        *scattered = Ray::new(rec.p, _direction);
+
         true
     }
 }
